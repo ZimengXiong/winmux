ZimengXiong/winmux

commit 97eed281b87b6e04c054aa3257be105138abddc1

1/2 "focused" means "globally focused" workspace/window/monitor
And nodes that we just "operate on" but not focused ones are just called
"targets". This will be documented in guide.adoc but I'm too lazy right now

Sources/AppBundle/focus.swift
@@ -55,9 +55,10 @@ var _focus: FrozenFocus = {
     return FrozenFocus(windowId: nil, workspaceName: monitor.activeWorkspace.name, monitorId: monitor.monitorId ?? 0)
 }()
 
-/// Global focus. This property must not be ever directly accessed from commands.
-/// Commands must firstly check --window-id, --workspace, AEROSPACE_FOCUSED_WINDOW_ID env and AEROSPACE_FOCUSED_WORKSPACE env
-/// before accessing global focus. todo make this property less easy to accidentially access from commands.
+/// Global focus.
+/// Commands must be cautious about accessing this property directly. There are legitimate cases.
+/// But, in general, commands must firstly check --window-id, --workspace, AEROSPACE_WINDOW_ID env and
+/// AEROSPACE_WORKSPACE env before accessing the global focus.
 var focus: LiveFocus { _focus.live }
 
 func setFocus(to newFocus: LiveFocus) -> Bool {
@@ -161,7 +162,7 @@ private func onWorkspaceChanged(_ oldWorkspace: String, _ newWorkspace: String) 
         process.executableURL = URL(filePath: exec)
         process.arguments = Array(config.execOnWorkspaceChange.dropFirst())
         var environment = config.execConfig.envVariables
-        environment["AEROSPACE_FOCUSED_WORKSPACE"] = newWorkspace
+        environment["AEROSPACE_WORKSPACE"] = newWorkspace
         environment["AEROSPACE_PREV_WORKSPACE"] = oldWorkspace
         process.environment = environment
         Result { try process.run() }.getOrThrow() // todo It's not perfect to fail here

Sources/AppBundle/command/CmdEnv.swift
@@ -29,10 +29,10 @@ struct CmdEnv: Copyable { // todo forward env from cli to server
             result["PWD"] = pwd
         }
         if let windowId {
-            result[AEROSPACE_FOCUSED_WINDOW_ID] = windowId.description
+            result[AEROSPACE_WINDOW_ID] = windowId.description
         }
         if let workspaceName {
-            result[AEROSPACE_FOCUSED_WORKSPACE] = workspaceName.description
+            result[AEROSPACE_WORKSPACE] = workspaceName.description
         }
         return result
     }

Sources/AppBundle/command/cmdResolveTargetOrReportError.swift
@@ -23,7 +23,7 @@ extension CmdArgs {
             if let wi = Window.get(byId: windowId) {
                 return wi.toLiveFocusOrReportError(io)
             } else {
-                io.err("Invalid <window-id> \(windowId) specified in \(AEROSPACE_FOCUSED_WINDOW_ID) env variable")
+                io.err("Invalid <window-id> \(windowId) specified in \(AEROSPACE_WINDOW_ID) env variable")
                 return nil
             }
         }

Sources/AppBundle/command/impl/ListMonitorsCommand.swift
@@ -6,10 +6,10 @@ struct ListMonitorsCommand: Command {
 
     func run(_ env: CmdEnv, _ io: CmdIo) -> Bool {
         check(Thread.current.isMainThread)
-        guard let target = args.resolveTargetOrReportError(env, io) else { return false }
+        let focus = focus
         var result = sortedMonitors
         if let focused = args.focused {
-            result = result.filter { (monitor) in (monitor.activeWorkspace == target.workspace) == focused }
+            result = result.filter { (monitor) in (monitor.activeWorkspace == focus.workspace) == focused }
         }
         if let mouse = args.mouse {
             let mouseWorkspace = mouseLocation.monitorApproximation.activeWorkspace

Sources/AppBundle/command/impl/ListWindowsCommand.swift
@@ -6,11 +6,11 @@ struct ListWindowsCommand: Command {
 
     func run(_ env: CmdEnv, _ io: CmdIo) -> Bool {
         check(Thread.current.isMainThread)
-        guard let target = args.resolveTargetOrReportError(env, io) else { return false }
+        let focus = focus
         var windows: [Window] = []
 
         if args.filteringOptions.focused {
-            if let window = target.windowOrNil {
+            if let window = focus.windowOrNil {
                 windows = [window]
             } else {
                 return io.err(noWindowIsFocused)
@@ -21,14 +21,14 @@ struct ListWindowsCommand: Command {
                 : args.filteringOptions.workspaces
                     .flatMap { filter in
                         switch filter {
-                            case .focused: [target.workspace]
+                            case .focused: [focus.workspace]
                             case .visible: Workspace.all.filter(\.isVisible)
                             case .name(let name): [Workspace.get(byName: name.raw)]
                         }
                     }
                     .toSet()
             if !args.filteringOptions.monitors.isEmpty {
-                let monitors: Set<CGPoint> = args.filteringOptions.monitors.resolveMonitors(io, target)
+                let monitors: Set<CGPoint> = args.filteringOptions.monitors.resolveMonitors(io)
                 if monitors.isEmpty { return false }
                 workspaces = workspaces.filter { monitors.contains($0.workspaceMonitor.rect.topLeftCorner) }
             }

Sources/AppBundle/command/impl/ListWorkspacesCommand.swift
@@ -6,13 +6,12 @@ struct ListWorkspacesCommand: Command {
 
     func run(_ env: CmdEnv, _ io: CmdIo) -> Bool {
         check(Thread.current.isMainThread)
-        guard let target = args.resolveTargetOrReportError(env, io) else { return false }
         var result: [Workspace] = Workspace.all
         if let visible = args.filteringOptions.visible {
             result = result.filter { $0.isVisible == visible }
         }
         if !args.filteringOptions.onMonitors.isEmpty {
-            let monitors: Set<CGPoint> = args.filteringOptions.onMonitors.resolveMonitors(io, target)
+            let monitors: Set<CGPoint> = args.filteringOptions.onMonitors.resolveMonitors(io)
             if monitors.isEmpty { return false }
             result = result.filter { monitors.contains($0.workspaceMonitor.rect.topLeftCorner) }
         }
@@ -32,11 +31,11 @@ struct ListWorkspacesCommand: Command {
 }
 
 extension [MonitorId] {
-    func resolveMonitors(_ io: CmdIo, _ target: LiveFocus) -> Set<CGPoint> {
+    func resolveMonitors(_ io: CmdIo) -> Set<CGPoint> {
         var requested: Set<CGPoint> = []
         let sortedMonitors = sortedMonitors
         for id in self {
-            let resolved = id.resolve(io, target, sortedMonitors: sortedMonitors)
+            let resolved = id.resolve(io, sortedMonitors: sortedMonitors)
             if resolved.isEmpty {
                 return []
             }
@@ -49,10 +48,10 @@ extension [MonitorId] {
 }
 
 extension MonitorId {
-    func resolve(_ io: CmdIo, _ target: LiveFocus, sortedMonitors: [Monitor]) -> [Monitor] {
+    func resolve(_ io: CmdIo, sortedMonitors: [Monitor]) -> [Monitor] {
         switch self {
             case .focused:
-                return [target.workspace.workspaceMonitor]
+                return [focus.workspace.workspaceMonitor]
             case .mouse:
                 return [mouseLocation.monitorApproximation]
             case .all:

Sources/AppBundle/util/appBundleUtil.swift
@@ -2,9 +2,8 @@ import AppKit
 import Common
 import Foundation
 
-// todo it's under the question if "focused" should be part of the name
-let AEROSPACE_FOCUSED_WINDOW_ID = "AEROSPACE_FOCUSED_WINDOW_ID" // env var
-let AEROSPACE_FOCUSED_WORKSPACE = "AEROSPACE_FOCUSED_WORKSPACE" // env var
+let AEROSPACE_WINDOW_ID = "AEROSPACE_WINDOW_ID" // env var
+let AEROSPACE_WORKSPACE = "AEROSPACE_WORKSPACE" // env var
 
 func stringType(of some: Any) -> String {
     let string = (some is Any.Type) ? String(describing: some) : String(describing: type(of: some))

commit 70007daa54bce1a8787cb571a6e2af444cc1ce4b

Fix AEROSPACE_FOCUSED_WORKSPACE env var in exec-on-workspace-change callback
https://github.com/nikitabobko/AeroSpace/issues/585

The bug was introduced in 97eed281b87b6e04c054aa3257be105138abddc1
because of inaccurate find and replace

Sources/AppBundle/focus.swift
@@ -162,7 +162,7 @@ private func onWorkspaceChanged(_ oldWorkspace: String, _ newWorkspace: String) 
         process.executableURL = URL(filePath: exec)
         process.arguments = Array(config.execOnWorkspaceChange.dropFirst())
         var environment = config.execConfig.envVariables
-        environment["AEROSPACE_WORKSPACE"] = newWorkspace
+        environment["AEROSPACE_FOCUSED_WORKSPACE"] = newWorkspace
         environment["AEROSPACE_PREV_WORKSPACE"] = oldWorkspace
         process.environment = environment
         Result { try process.run() }.getOrThrow() // todo It's not perfect to fail here