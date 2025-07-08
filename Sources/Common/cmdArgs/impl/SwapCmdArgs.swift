nikitabobko/AeroSpace

Implement the swap command.
`aerospace swap` swaps the currently focused window with a window in a
cardinal direction or with the next/prev window in the depth first order
of windows in the workspace.

Target window selection works identically to the focus command (so the
cardinal directions respect MRU).

_fixes https://github.com/nikitabobko/AeroSpace/issues/8

Sources/Common/cmdArgs/impl/SwapCmdArgs.swift
@@ -1,0 +1,29 @@
+public struct SwapCmdArgs: CmdArgs {
+    public let rawArgs: EquatableNoop<[String]>
+    public init(rawArgs: [String]) { self.rawArgs = .init(rawArgs) }
+    public static let parser: CmdParser<Self> = cmdParser(
+        kind: .swap,
+        allowInConfig: true,
+        help: swap_help_generated,
+        options: [
+            "--swap-focus": trueBoolFlag(\.swapFocus),
+            "--wrap-around": trueBoolFlag(\.wrapAround),
+        ],
+        arguments: [newArgParser(\.target, parseFocusTargetArg, mandatoryArgPlaceholder: "(left|down|up|right|dfs-next|dfs-prev)")]
+    )
+
+    public var target: Lateinit<FocusTargetArg> = .uninitialized
+    public var swapFocus: Bool = false
+    public var wrapAround: Bool = false
+    public var windowId: UInt32?
+    public var workspaceName: WorkspaceName?
+
+    public init(rawArgs: [String], target: FocusTargetArg) {
+        self.rawArgs = .init(rawArgs)
+        self.target = .initialized(target)
+    }
+}
+
+public func parseSwapCmdArgs(_ args: [String]) -> ParsedCmd<SwapCmdArgs> {
+    return parseSpecificCmdArgs(SwapCmdArgs(rawArgs: args), args)
+}

ZimengXiong/winmux

Sources/Common/cmdArgs/impl/SwapCmdArgs.swift
@@ -1,0 +1,29 @@
+public struct SwapCmdArgs: CmdArgs {
+    public let rawArgs: EquatableNoop<[String]>
+    public init(rawArgs: [String]) { self.rawArgs = .init(rawArgs) }
+    public static let parser: CmdParser<Self> = cmdParser(
+        kind: .swap,
+        allowInConfig: true,
+        help: swap_help_generated,
+        options: [
+            "--swap-focus": trueBoolFlag(\.swapFocus),
+            "--wrap-around": trueBoolFlag(\.wrapAround),
+        ],
+        arguments: [newArgParser(\.target, parseCardinalOrDfsDirection, mandatoryArgPlaceholder: CardinalOrDfsDirection.unionLiteral)],
+    )
+
+    public var target: Lateinit<CardinalOrDfsDirection> = .uninitialized
+    public var swapFocus: Bool = false
+    public var wrapAround: Bool = false
+    public var windowId: UInt32?
+    public var workspaceName: WorkspaceName?
+
+    public init(rawArgs: [String], target: CardinalOrDfsDirection) {
+        self.rawArgs = .init(rawArgs)
+        self.target = .initialized(target)
+    }
+}
+
+public func parseSwapCmdArgs(_ args: [String]) -> ParsedCmd<SwapCmdArgs> {
+    return parseSpecificCmdArgs(SwapCmdArgs(rawArgs: args), args)
+}