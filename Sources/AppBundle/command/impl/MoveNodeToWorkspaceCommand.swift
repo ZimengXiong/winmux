ZimengXiong/winmux

commit 3987407488e9b64a3981dfc4c3266cc241e98956

Refactor MoveNodeToMonitorCommand implementation

Sources/AppBundle/command/impl/MoveNodeToWorkspaceCommand.swift
@@ -23,13 +23,17 @@ struct MoveNodeToWorkspaceCommand: Command {
             case .direct(let name):
                 targetWorkspace = Workspace.get(byName: name.raw)
         }
-        if subjectWs == targetWorkspace {
-            io.err("Window '\(window.windowId)' already belongs to workspace '\(targetWorkspace.name)'. Tip: use --fail-if-noop to exit with non-zero code")
-            return !args.failIfNoop
-        }
-        let targetContainer: NonLeafTreeNodeObject = window.isFloating ? targetWorkspace : targetWorkspace.rootTilingContainer
-
-        window.bind(to: targetContainer, adaptiveWeight: WEIGHT_AUTO, index: INDEX_BIND_LAST)
-        return args.focusFollowsWindow ? window.focusWindow() : true
+        return moveWindowToWorkspace(window, targetWorkspace, io, focusFollowsWindow: args.focusFollowsWindow, failIfNoop: args.failIfNoop)
     }
+}
+
+@MainActor
+func moveWindowToWorkspace(_ window: Window, _ targetWorkspace: Workspace, _ io: CmdIo, focusFollowsWindow: Bool, failIfNoop: Bool) -> Bool {
+    if window.nodeWorkspace == targetWorkspace {
+        io.err("Window '\(window.windowId)' already belongs to workspace '\(targetWorkspace.name)'. Tip: use --fail-if-noop to exit with non-zero code")
+        return !failIfNoop
+    }
+    let targetContainer: NonLeafTreeNodeObject = window.isFloating ? targetWorkspace : targetWorkspace.rootTilingContainer
+    window.bind(to: targetContainer, adaptiveWeight: WEIGHT_AUTO, index: INDEX_BIND_LAST)
+    return focusFollowsWindow ? window.focusWindow() : false
 }

Sources/AppBundle/command/impl/MoveCommand.swift
@@ -63,7 +63,7 @@ struct MoveCommand: Command {
             }
 
             let moveNodeToMonitorArgs = MoveNodeToMonitorCmdArgs(target: .directional(direction))
-                .copy(\.moveNodeToWorkspace.focusFollowsWindow, true)
+                .copy(\.focusFollowsWindow, true)
 
             return MoveNodeToMonitorCommand(args: moveNodeToMonitorArgs).run(env, io)
     }

Sources/AppBundle/command/impl/MoveNodeToMonitorCommand.swift
@@ -14,10 +14,13 @@ struct MoveNodeToMonitorCommand: Command {
         }
         switch args.target.val.resolve(currentMonitor, wrapAround: args.wrapAround) {
             case .success(let targetMonitor):
-                let wName = WorkspaceName.parse(targetMonitor.activeWorkspace.name).getOrNil(appendErrorTo: &io.stderr)
-                guard let wName else { return false }
-                let moveNodeToWorkspace = args.moveNodeToWorkspace.copy(\.target, .initialized(.direct(wName)))
-                return MoveNodeToWorkspaceCommand(args: moveNodeToWorkspace).run(env, io)
+                return moveWindowToWorkspace(
+                    window,
+                    targetMonitor.activeWorkspace,
+                    io,
+                    focusFollowsWindow: true,
+                    failIfNoop: args.failIfNoop
+                )
             case .failure(let msg):
                 return io.err(msg)
         }

Sources/AppBundleTests/command/MoveNodeToMonitorCommandTest.swift
@@ -8,7 +8,7 @@ final class MoveNodeToMonitorCommandTest: XCTestCase {
 
     func testParse() {
         testParseCommandSucc("move-node-to-monitor next", MoveNodeToMonitorCmdArgs(target: .relative(.next)))
-        testParseCommandSucc("move-node-to-monitor --fail-if-noop main", MoveNodeToMonitorCmdArgs(target: .patterns([.main])).copy(\.moveNodeToWorkspace.failIfNoop, true))
+        testParseCommandSucc("move-node-to-monitor --fail-if-noop main", MoveNodeToMonitorCmdArgs(target: .patterns([.main])).copy(\.failIfNoop, true))
         assertEquals(parseCommand("move-node-to-monitor --fail-if-noop next").errorOrNil, "--fail-if-noop is incompatible with (left|down|up|right|next|prev)")
         assertEquals(parseCommand("move-node-to-monitor --fail-if-noop left").errorOrNil, "--fail-if-noop is incompatible with (left|down|up|right|next|prev)")
     }

Sources/Common/cmdArgs/impl/MoveNodeToMonitorCmdArgs.swift
@@ -9,10 +9,9 @@ public struct MoveNodeToMonitorCmdArgs: CmdArgs {
             // "Own" option
             "--wrap-around": trueBoolFlag(\.wrapAround),
 
-            // Forward to moveNodeToWorkspace
             "--window-id": optionalWindowIdFlag(),
-            "--focus-follows-window": trueBoolFlag(\.moveNodeToWorkspace.focusFollowsWindow),
-            "--fail-if-noop": trueBoolFlag(\.moveNodeToWorkspace.failIfNoop),
+            "--focus-follows-window": trueBoolFlag(\.focusFollowsWindow),
+            "--fail-if-noop": trueBoolFlag(\.failIfNoop),
         ],
         arguments: [newArgParser(\.target, parseTarget, mandatoryArgPlaceholder: MonitorTarget.cases.joinedCliArgs)]
     )
@@ -22,13 +21,11 @@ public struct MoveNodeToMonitorCmdArgs: CmdArgs {
         self.target = .initialized(target)
     }
 
+    /*conforms*/ public var windowId: UInt32?
     /*conforms*/ public var workspaceName: WorkspaceName?
-    /*conforms*/ public var windowId: UInt32? { // Forward to moveNodeToWorkspace
-        get { moveNodeToWorkspace.windowId }
-        set(newValue) { moveNodeToWorkspace.windowId = newValue }
-    }
 
-    public var moveNodeToWorkspace = MoveNodeToWorkspaceCmdArgs(rawArgs: [])
+    public var failIfNoop: Bool = false
+    public var focusFollowsWindow: Bool = false
     public var wrapAround: Bool = false
     public var target: Lateinit<MonitorTarget> = .uninitialized
 }
@@ -36,5 +33,5 @@ public struct MoveNodeToMonitorCmdArgs: CmdArgs {
 public func parseMoveNodeToMonitorCmdArgs(_ args: [String]) -> ParsedCmd<MoveNodeToMonitorCmdArgs> {
     parseSpecificCmdArgs(MoveNodeToMonitorCmdArgs(rawArgs: args), args)
         .filter("--wrap-around is incompatible with <monitor-pattern> argument") { $0.wrapAround.implies(!$0.target.val.isPatterns) }
-        .filter("--fail-if-noop is incompatible with \(MonitorTarget.casesExceptPatterns.joinedCliArgs)") { $0.moveNodeToWorkspace.failIfNoop.implies($0.target.val.isPatterns) }
+        .filter("--fail-if-noop is incompatible with \(MonitorTarget.casesExceptPatterns.joinedCliArgs)") { $0.failIfNoop.implies($0.target.val.isPatterns) }
 }

commit 9987e8cac03248ddb682fd343b50cb99c841ae45

Fix incorrect MoveNodeToWorkspaceCommand exit code
https://github.com/nikitabobko/AeroSpace/issues/1563

The regression got introduced in 3987407488e9b64a3981dfc4c3266cc241e98956
Apparently, I was drunk

Sources/AppBundle/command/impl/MoveNodeToWorkspaceCommand.swift
@@ -36,5 +36,5 @@ func moveWindowToWorkspace(_ window: Window, _ targetWorkspace: Workspace, _ io:
     }
     let targetContainer: NonLeafTreeNodeObject = window.isFloating ? targetWorkspace : targetWorkspace.rootTilingContainer
     window.bind(to: targetContainer, adaptiveWeight: WEIGHT_AUTO, index: index)
-    return focusFollowsWindow ? window.focusWindow() : false
+    return focusFollowsWindow ? window.focusWindow() : true
 }